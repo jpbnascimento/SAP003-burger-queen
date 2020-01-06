3c081cb86d7fc34830282da64a08b4b31928b65d
Jessica Patricia <jessicapatricia03@hotmail.com>
2020-01-06 20:44:49 -0300

    funcao deletar correto

--- burger-queen/src/Components/Header/header.js
+++ burger-queen/src/Components/Header/header.js
@@ -1,21 +1,49 @@
 import React, { useState, useEffect } from "react";
 import firebaseApp from "../../Firebase";
 import "./header.scss";
+import logo from "./Pasta Sem Título/logo-2.svg"
 
 const Header = () => {
-  const [pedido, setPedidos] = useState([]); // 2 - mudar para dicionario
+  const [pedido, setPedidos] = useState([]);
   const [tipoMenu, setTipoMenu] = useState([]);
   const [menuAtivo, setMenuAtivo] = useState("lanche");
 
+console.log(pedido);
 
-  function incluirPedidos(event) {
-    const pedido1 = event.target.innerHTML; // 1 - passar op parametro ex: <p id={index} onClick={()=>{setMenuAtivo(item)}}>
-
-    setPedidos([...pedido, pedido1]);  // 2 - mudar para dicionario
+  function incluirPedidos(item) {
+    if(pedido.some(el => el.nome === item.nome)){
+      console.log(item.contador)
+      item.contador++
+      setPedidos([...pedido]);
+    } else {
+      item.contador = 1
+      setPedidos([...pedido, item]);
+    }
   }
 
-  function excluirPedido(event) { // 3 - Decrementar o contador do dicionario TAMBEM usar parametro
+  function decremento(item) {
+    if(item.contador == 1){
+      const qualquer = pedido.filter(minie => {
+        return minie != item;
+      });
+      setPedidos(qualquer);
+
+    } else {
+      item.contador --
+      setPedidos([...pedido]);
+    }
+  }
+
+  function incremento(item) {
+   
+      item.contador ++
+      setPedidos([...pedido]);
+    
+  }
+
+  function excluirPedido(event) {
     const excluido = pedido[event.target.parentNode.id];
+    console.log(excluido);
     const qualquer = pedido.filter(item => {
       return item != excluido;
     });
@@ -23,30 +51,31 @@ const Header = () => {
     setPedidos(qualquer);
   }
 
-// 4 - Salvar dicionario de pedidos com mesa e nome do usuarione E  status
-
   const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    firebaseApp
-      .collection("menu")
-      .get()
-      .then(snap => {
-        const newItems = snap.docs.map(doc => ({
-          ...doc.data()
-        }));
-        const temptipos = snap.docs.map(doc => {
-          return doc.data()["menu"];
+    useEffect(() => {
+      firebaseApp
+        .collection("menu")
+        .get()
+        .then(snap => {
+          const newItems = snap.docs.map(doc => ({
+            ...doc.data()
+          }));
+          const temptipos = snap.docs.map(doc => {
+            return doc.data()["menu"];
+          });
+          const tipos = [...new Set(temptipos)];
+          // console.log(tipos);
+          setItems(newItems);
+          setTipoMenu(tipos);
         });
-        const tipos = [...new Set(temptipos)];
-        console.log(tipos);
-        setItems(newItems);
-        setTipoMenu(tipos);
-      });
-  }, []);
+    }, []);
+
+    const total = pedido.reduce((acc, item)=> acc + (item.contador * item.valor), 0)
 
   return (
     <>
+      <img className="logo" src={logo}></img>
       <div className="tippMenu">
         {tipoMenu.map((item, index) => (
           <p  id={index} onClick={()=>{setMenuAtivo(item)}}>
@@ -57,12 +86,12 @@ const Header = () => {
       <div className="header">
         {items.map((item, index) => (
           (item.menu === menuAtivo && 
-          <div className="itens" onClick={incluirPedidos}>
+          <div className="itens" onClick={() => incluirPedidos(item)}>
             <p>
               {item.nome} 
-            </p>
-            <p>
-             R$ {item.valor}
+              {item.adicional?item.adicional.map((i) => <label><input name="adicional" type="radio"/>{i}</label>): "" }
+        {item.opcoes?item.opcoes.map((i, index) => <label><input name="opcao" type="radio"/>{i}</label>): "" }
+              R$ {item.valor},00
             </p>
           </div>
           )
@@ -70,14 +99,25 @@ const Header = () => {
       </div>
       <div className="pedido">
         <strong>Pedido:</strong>
+        <label>Cliente: </label>
+        <input type="text"></input>
+        <label>Mesa: </label>
+        <input type="number"></input>
         <div>
           {pedido.map((item, index) => (
             <p id={index}>
-              {item}
+              {item.nome}
+             R$  {item.valor},00
               <button onClick={excluirPedido}>X</button>
+              <button onClick={() => incremento(item)}> + </button>
+              <span> {item.contador} </span>
+              <button onClick={() => decremento(item)}> - </button>
+         
             </p>
           ))}
         </div>
+          <span>{total}</span>
+          <button>Enviar Pedido</button>
       </div>
       
     </>
